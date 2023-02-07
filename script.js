12658f3b10883a5035a671932c9b71cc4d237bd3
Fuga14 <vladyslavpereder@gmail.com>
2023-02-08 00:40:52 +0200

    v1.0.0

--- script.js
+++ script.js
@@ -76,8 +76,13 @@ class App {
   #workouts = [];
   #mapZoomLevel = 13;
   constructor() {
+    // Get user's position
     this._getPosition();
 
+    //Get data from local storage
+    this._getLocalStorage();
+
+    //Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
 
@@ -99,8 +104,6 @@ class App {
     const { longitude } = position.coords; // довгота
     const coords = [latitude, longitude];
 
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -110,6 +113,10 @@ class App {
 
     //Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -190,6 +197,9 @@ class App {
 
     // Hide form + clear input fields
     this._hideForm();
+
+    //Set local storage to all new workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -265,7 +275,6 @@ class App {
 
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
-    console.log(workoutEl);
 
     if (!workoutEl) return;
     const workout = this.#workouts.find(
@@ -277,6 +286,27 @@ class App {
         duration: 0.5,
       },
     });
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
